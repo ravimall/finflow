f7333e9ad3d6c3eb67b1a59ba5f9a8326f6af391
ravimall <ravi.rasbrands@gmail.com>
2025-10-07 13:48:29 +0530

    change to passport.use

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -17,29 +17,37 @@ function generateToken(user) {
   );
 }
 
-passport.use(new GoogleStrategy(
-  {
-    clientID: process.env.GOOGLE_CLIENT_ID || "",
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-    callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:5000/api/users/google/callback"
-  },
-  async (accessToken, refreshToken, profile, done) => {
-    try {
-//       const [user, created] = await User.findOrCreate({
-        where: { google_sub: profile.id }
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      callbackURL:
+        process.env.GOOGLE_CALLBACK_URL ||
+        "http://localhost:5000/api/users/google/callback"
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const [user, created] = await User.findOrCreate({
+          where: { google_sub: profile.id },
           defaults: {
-          name: profile.displayName,
-          email: (profile.emails && profile.emails[0] && profile.emails[0].value) || "",
-          role: "agent",
-          google_sub: profile.id
-        }
-      });
-      return done(null, user);
-    } catch (err) {
-      return done(err, null);
+            name: profile.displayName,
+            email:
+              (profile.emails &&
+                profile.emails[0] &&
+                profile.emails[0].value) ||
+              "",
+            role: "agent",
+            google_sub: profile.id
+          }
+        });
+        return done(null, user);
+      } catch (err) {
+        return done(err, null);
+      }
     }
-  }
-));
+  )
+);
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
